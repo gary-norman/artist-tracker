d791297243c20218f27f58819ed69b0132a89d61
Gary Norman <gary.norman.th@gmail.com>
2024-05-15 18:06:27 +0100

    fix: json is parsing correctly into struct

--- api/database.go
+++ api/database.go
@@ -2,68 +2,98 @@ package api
 
 import (
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
-	"time"
 )
 
 type Artist struct {
-	Id           string
-	Image        string
-	Name         string
-	Members      []string
-	CreationDate string
-	FirstAlbum   string
-	Locations    string
-	ConcertDates string
-	Relations    string
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Relations    string   `json:"relations"`
 }
 
-type DatesLocations struct {
-	Location string
-	date     []string
-}
+//type DatesLocations struct {
+//	Location string   `json:"location"`
+//	Date     []string `json:"date"`
+//}
+//
+//type Relations struct {
+//	Id int64 `json:"id"`
+//	DatesLocations
+//}
 
-type Relations struct {
-	Id int64
-	DatesLocations
-}
+//var myClient = &http.Client{Timeout: 10 * time.Second}
 
-var myClient = &http.Client{Timeout: 10 * time.Second}
-
-func getJson(url string, target any) error {
-	r, err := myClient.Get(url)
+// getJson function fetches JSON data from a URL and decodes it into a target variable
+func getJson(url string, target interface{}) error {
+	r, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("Unable to read JSON file due to %s", err)
 		return err
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
+	defer func() {
+		if err := r.Body.Close(); err != nil {
 			log.Fatalf("Unable to close connection to JSON file due to %s", err)
 		}
-	}(r.Body)
+	}()
+
 	return json.NewDecoder(r.Body).Decode(target)
 }
 
-func AllJsonToStruct() Artist {
-	artistJson := Artist{}
-	err2 := getJson("https://groupietrackers.herokuapp.com/api/artists", &artistJson)
-	if err2 != nil {
-		log.Fatalf("Unable to download JSON due to %s", err2)
+// AllJsonToStruct function fetches all artist data and returns a slice of Artist structs
+func AllJsonToStruct() []Artist {
+	var artists []Artist
+	err := getJson("https://groupietrackers.herokuapp.com/api/artists", &artists)
+	if err != nil {
+		log.Fatalf("Unable to create struct due to %s", err)
 	}
-	return artistJson
+	return artists
 }
 
-func RelationJsonToStruct(id string) Relations {
-	relationJson := Relations{}
-	err2 := getJson("https://groupietrackers.herokuapp.com/api/relation/"+id, &relationJson)
-	if err2 != nil {
-		log.Fatalf("Unable to download JSON due to %s", err2)
-	}
-	return relationJson
-}
+//func createdb() {
+//	url := "https://groupietrackers.herokuapp.com/api/artists"
+//
+//	// Fetch the data
+//	resp, err := http.Get(url)
+//	if err != nil {
+//		fmt.Println("Error fetching data:", err)
+//		os.Exit(1)
+//	}
+//	defer func(Body io.ReadCloser) {
+//		err := Body.Close()
+//		if err != nil {
+//			log.Fatalf("Unable to close connection to JSON file due to %s", err)
+//		}
+//	}(resp.Body)
+//
+//	// Check for HTTP errors
+//	if resp.StatusCode != http.StatusOK {
+//		fmt.Printf("Error: status code %d\n", resp.StatusCode)
+//		os.Exit(1)
+//	}
+//
+//	// Decode the JSON data into the struct
+//	var artists Artist
+//	if err := json.NewDecoder(resp.Body).Decode(&artists); err != nil {
+//		fmt.Println("Error decoding JSON:", err)
+//		os.Exit(1)
+//	}
+//
+//	// Print the struct
+//	fmt.Printf("User: %+v\n", artists)
+//}
+
+//func RelationJsonToStruct(id string) Relations {
+//	relationJson := Relations{}
+//	err2 := getJson("https://groupietrackers.herokuapp.com/api/relation/"+id, &relationJson)
+//	if err2 != nil {
+//		log.Fatalf("Unable to download JSON due to %s", err2)
+//	}
+//	return relationJson
+//}
 
 //func SearchArtist(artist string) Artist {
 //	allData := AllJsonToStruct()
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "artist-tracker/api"
+import (
+	"artist-tracker/api"
+	"log"
+)
 
 func main() {
+	// Call AllJsonToStruct and print the result
+	artists := api.AllJsonToStruct()
+	for _, artist := range artists {
+		log.Printf("Artist: %+v\n", artist)
+	}
 	api.HandleRequests()
 }
