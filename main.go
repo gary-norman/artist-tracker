d2b08b75eaecb1d71dc4ae5c109aad554958794b
kornal <kamilornal@gmail.com>
2024-05-22 20:14:36 +0100

    testing

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	// Call AllJsonToStruct and print the result
 	artists := api.AllJsonToStruct("https://groupietrackers.herokuapp.com/api/artists")
 	api.UpdateArtistInfo(artists)
-	api.HandleRequests(artists)
+	api.HandleRequests(artists, api.GetTemplate())
 	//for _, artist := range artists {
 	//	fmt.Printf("ID for %v: %s\n", artist.Name, api.SearchArtistByName(artist.Name))
 	//	fmt.Printf("Release for %v: %s\n", artist.Name, api.GetReleasesByArtistID(api.SearchArtistByName(artist.Name)))
