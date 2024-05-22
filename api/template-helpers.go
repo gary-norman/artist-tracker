105b6df839efa15cdc623f2e94d6950e51dff6b2
kornal <kamilornal@gmail.com>
2024-05-22 19:58:51 +0100

    adding template helpers to run custom functions on the html go template

---
+++ api/template-helpers.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"html/template"
+	"math/rand"
+	"time"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(template.FuncMap{
+		"random":    RandomInt,
+		"increment": Increment,
+		"decrement": Decrement,
+	}).ParseGlob("templates/*.html"))
+}
+
+func RandomInt(max int) int {
+	return rand.Intn(max)
+}
+
+func Increment(n int) int {
+	return n + 1
+}
+
+func Decrement(n int) int {
+	return n - 1
+}
+
+// Function to shuffle a slice of integers
+func Shuffle(slice []int) {
+	rand.Seed(time.Now().UnixNano())
+	for i := len(slice) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+func GetTemplate() *template.Template {
+	return tpl
+}
