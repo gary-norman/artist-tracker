eca4c8f017f7ec713bc3a088b0fac4ea53cd72a1
Gary Norman <gary.norman.th@gmail.com>
2024-05-24 02:19:37 +0100

    feat: fetching TADB artist info completes successfully

--- main.go
+++ main.go
@@ -15,8 +15,7 @@ func main() {
 		api.ProcessAudioDbArtist(&artists[i], artists[i].Name, tadbArtist[i].Id, err)
 	}
 	//api.HandleRequests(artists, api.GetTemplate())
-	for _, artist := range artists {
-		fmt.Println(artist)
+		fmt.Println(artists[0])
 		//fmt.Printf("ID for %v: %s\n", artist.Name, api.SearchArtistByName(artist.Name))
 		//fmt.Printf("Release for %v: %s\n", artist.Name, api.GetReleasesByArtistID(api.SearchArtistByName(artist.Name)))
 
