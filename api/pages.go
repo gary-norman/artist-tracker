0b8258375678cadc1abb6c1ad3aa8a9a85d9f5ba
rinrino <rinrino1127@gmail.comm>
2024-06-18 12:48:50 +0100

    add artist link for render aritst page, and basic artistHandler just to see how it wroks

--- api/pages.go
+++ api/pages.go
@@ -44,3 +44,33 @@ func HomePage(w http.ResponseWriter, r *http.Request, artists []Artist, tpl *tem
 		return
 	}
 }
+
+func ArtistHandler(w http.ResponseWriter, r *http.Request, artists []Artist, tpl *template.Template) {
+
+	t := tpl.Lookup("artist.html")
+	if t == nil {
+		ErrorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+
+	// later Gary can modify more, to read from the URl id number
+	artstExample, _ := SearchArtist(artists, "Rihanna")
+
+	err := t.Execute(w, &artstExample)
+	if err != nil {
+		var e Error
+		switch {
+		case errors.As(err, &e):
+
+			fmt.Println("\nerr is:", err, "\nerrrr is:", err.Error())
+
+			ErrorHandler(w, r, e.Status())
+
+		default:
+			fmt.Println("err is:", err, "errrr is:", err.Error())
+			ErrorHandler(w, r, http.StatusInternalServerError)
+		}
+		return
+	}
+
+}
