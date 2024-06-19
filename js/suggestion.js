e2e76ce6add1d7cb32e23c3a35789cfe42b30faf
Gary Norman <gary.norman.th@gmail.com>
2024-06-19 14:38:29 +0100

    fix: cleaned up code, removed unnecessary functions, added artist pics on artist page

--- js/suggestion.js
+++ js/suggestion.js
@@ -84,44 +84,46 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         suggestionsData.forEach(function (suggestion) {
+            let artistName;
+            let contentText;
+            let content;
             const a = document.createElement('a');
             a.setAttribute('href', `/artist?name=${encodeURIComponent(suggestion.artist.name)}`);
             a.dataset.artistName = suggestion.artist.name;
-            a.classList.add('artist-link');
 
             if (suggestion.category === 'Concert' && suggestion.matchitem && suggestion.matchitem.dates) {
-                var content = document.createElement('div');
+                content = document.createElement('div');
                 content.className = 'content';
 
-                var dateDiv = document.createElement('div');
+                const dateDiv = document.createElement('div');
                 dateDiv.className = 'pic date';
 
-                var monthYearDiv = document.createElement('div');
+                const monthYearDiv = document.createElement('div');
                 monthYearDiv.className = 'month-year';
 
-                var monthDiv = document.createElement('div');
+                const monthDiv = document.createElement('div');
                 monthDiv.textContent = suggestion.matchitem.dates.Month;
 
-                var yearDiv = document.createElement('div');
+                const yearDiv = document.createElement('div');
                 yearDiv.textContent = suggestion.matchitem.dates.Year;
 
                 monthYearDiv.appendChild(monthDiv);
                 monthYearDiv.appendChild(yearDiv);
 
-                var dayDiv = document.createElement('div');
+                const dayDiv = document.createElement('div');
                 dayDiv.textContent = suggestion.matchitem.dates.Day;
 
                 dateDiv.appendChild(dayDiv);
                 dateDiv.appendChild(monthYearDiv);
 
-                var contentText = document.createElement('div');
+                contentText = document.createElement('div');
                 contentText.className = 'content-text go-down-home';
 
-                var artistName = document.createElement('div');
+                artistName = document.createElement('div');
                 artistName.className = 'p--bold cut concert';
                 artistName.textContent = suggestion.artist && suggestion.artist.name ? suggestion.artist.name : 'Unknown Artist';
 
-                var locationName = document.createElement('div');
+                const locationName = document.createElement('div');
                 locationName.className = 'small light cut concert';
                 locationName.textContent = suggestion.matchitem.location || 'Unknown Location';
 
@@ -138,10 +140,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     console.warn(`Category ${suggestion.category} not found in categories object.`);
                 }
             } else {
-                var content = document.createElement('div');
+                content = document.createElement('div');
                 content.className = 'content';
 
-                var img = document.createElement('img');
+                const img = document.createElement('img');
                 if (suggestion.category === 'Album') {
                     img.className = 'pic album';
                     img.src = suggestion.artist.strAlbumThumb ? suggestion.artist.strAlbumThumb : 'default-album-image-url.jpg';
@@ -152,35 +154,21 @@ document.addEventListener('DOMContentLoaded', function () {
                     img.alt = 'Profile image of ' + (suggestion.artist && suggestion.artist.name ? suggestion.artist.name : 'Unknown Artist');
                 }
 
-                var contentText = document.createElement('div');
+                contentText = document.createElement('div');
                 contentText.className = 'content-text go-down';
 
-                var boldCut = document.createElement('div');
+                let boldCut = document.createElement('div');
                 boldCut.className = 'p--bold cut';
                 boldCut.textContent = suggestion.matchitem || '';
 
+
+
                 if (suggestion.category === 'Artist' && suggestion.matchitem && suggestion.matchitem.toLowerCase() === suggestion.artist.name.toLowerCase()) {
                     contentText.appendChild(boldCut);
-                } else if (suggestion.category === 'Member' && suggestion.matchitem) {
-                    if (suggestion.artist.Members !== "") {
-                        if (suggestion.matchitem.toLowerCase() === suggestion.artist.name.toLowerCase()) {
+                } else if (suggestion.category === 'Member' && suggestion.matchitem && suggestion.artist.Members !== "" && suggestion.matchitem.toLowerCase() === suggestion.artist.name.toLowerCase()) {
                             contentText.appendChild(boldCut);
-                        } else {
-                            var artistName = document.createElement('div');
-                            artistName.className = 'p--bold cut';
-                            artistName.textContent = suggestion.artist && suggestion.artist.name ? suggestion.artist.name : 'Unknown Artist';
-                            contentText.appendChild(boldCut);
-                            contentText.appendChild(artistName);
-                        }
-                    } else {
-                        var artistName = document.createElement('div');
-                        artistName.className = 'p--normal';
-                        artistName.textContent = suggestion.artist && suggestion.artist.name ? suggestion.artist.name : 'Unknown Artist';
-                        contentText.appendChild(boldCut);
-                        contentText.appendChild(artistName);
-                    }
                 } else {
-                    var artistName = document.createElement('div');
+                    artistName = document.createElement('div');
                     artistName.className = 'p--normal';
                     artistName.textContent = suggestion.artist && suggestion.artist.name ? suggestion.artist.name : 'Unknown Artist';
                     contentText.appendChild(boldCut);
@@ -210,39 +198,4 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
-
-    async function loadArtistPage(artistName) {
-        try {
-            const response = await fetch(`/artist?name=${encodeURIComponent(artistName)}`);
-            if (response.ok) {
-                const artistPageContent = await response.text();
-                // this part seem never reach....
-                console.log("response from artstPage:",artistPageContent)
-               /*  const mainContent = document.getElementById('main-content');
-                if (mainContent) {
-                    mainContent.innerHTML = artistPageContent;
-                    // Update the URL without reloading the page
-                    history.pushState(null, '', `/artist?name=${encodeURIComponent(artistName)}`);
-                } else {
-                    console.error('Element with id "main-content" not found.');
-                } */
-            } else {
-                console.error('Failed to load artist page');
-            }
-        } catch (error) {
-            console.error('Error loading artist page:', error);
-        }
-    }
-
-    document.addEventListener('click', function (e) {
-        if (e.target.classList.contains('artist-link')) {
-            e.preventDefault();
-            const artistName = e.target.dataset.artistName;
-            if (artistName) {
-                loadArtistPage(artistName);
-            } else {
-                console.error('Artist name not found in dataset.');
-            }
-        }
-    });
 });
