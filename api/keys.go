1cf9ae7935cc636567a5b30628766bcc62d23423
kornal <kamilornal@gmail.com>
2024-06-24 19:42:18 +0100

    feat: added keys.go file to store auth keys for api's

---
+++ api/keys.go
@@ -0,0 +1,13 @@
+package api
+
+// spotify
+var spotifyAuthToken = "BQD72yS-ec-KHOIOkuI5Yk8wWjxxEkN5rqfX_3myERzfQs1aY7FPkZajomH6nFJeSCeQTx1sEqzuzV4A5hxu5UsfdHs28x49X_5Y9erd0N2fKxS4ytM"
+
+// mapbox
+const (
+	username    = "loreworld"
+	accessToken = "sk.eyJ1IjoibG9yZXdvcmxkIiwiYSI6ImNsd3FtMmVnZTAzbzMybHM3djg5eGI0dXoifQ.ApBPbrjmry0mcYTOJtvXsQ"
+)
+
+// rapidAPI
+var apiKey = "dccdb0a36amsh783e1cc91e71909p1fadc0jsn9c03dce7a6cd"
