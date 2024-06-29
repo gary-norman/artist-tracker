8c3b1ece6964a56a9b1633513b4f172eaf8360cb
Gary Norman <gary.norman.th@gmail.com>
2024-06-29 09:53:20 +0100

    feat: hover on first album gives provide api date

---
+++ js/artist.js
@@ -0,0 +1,9 @@
+const firstalbum = document.getElementById('first-album')
+firstalbum.addEventListener('mouseover', ()=> {
+    document.getElementById('createSwitch').innerText = "original api gives: " + document.getElementById
+    ("albumInfo").getAttribute("data-album");
+})
+firstalbum.addEventListener('mouseout', ()=> {
+    document.getElementById('createSwitch').innerText = document.getElementById
+    ("actualAlbumInfo").getAttribute("data-album");
+})
